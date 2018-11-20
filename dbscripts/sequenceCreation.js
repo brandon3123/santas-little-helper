99819663ef554517181f401d10d4265abd634412
Brandon <brandonnolan3123@gmail.com>
2018-11-20 09:39:28 -0700

    Add files via upload

---
+++ dbscripts/sequenceCreation.js
@@ -0,0 +1,1 @@
+db.counters.insert({_id:"user_id",sequence_value:0})
---
+++ models/user.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var mongooseUniqueValidator = require('mongoose-unique-validator');
+
+var userSchema = new Schema({
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
+    password: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    dateOfBirth: {type: Date , required: true},
+    address: [{type: Schema.Types.ObjectId, ref: 'Address'}]
+});
+
+userSchema.plugin(mongooseUniqueValidator)
+
+module.exports = mongoose.model('User', userSchema);
