ec6eba36ee5a84420f4fb604f99d433073c817e3
Brandon <brandonnolan3123@gmail.com>
2018-11-20 09:37:45 -0700

    Add files via upload

---
+++ routes/login.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var body = require('body-parser');
+var router = express.Router();
+var loginService = require('../services/LoginService');
+
+router.get('/', function (req, res, next) {
+    res.render('login', {
+    loginHeader: 'Login',
+    css: ['stylesheets/loginStyle.css']});
+});
+
+router.post('/attemptLogin', function (req, res, next) {
+    var userName = req.body.userName;
+    var password = req.body.password;
+    var loginSuccess = userLogin(userName, password);
+    res.redirect('index');
+    // if (loginSuccess) {
+    //     res.redirect('index');
+    // }
+    // res.redirect('/')
+});
+
+function userLogin(userName, password) {
+    var loginSuccess = false;
+    if (userName && password) {
+        if (userName == 'brandon' && password == 'pw') {
+            loginSuccess = true;
+        }
+    }
+    return loginSuccess;
+};
+
+module.exports = router;
